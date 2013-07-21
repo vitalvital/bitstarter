dc23e830e3998d7b25fc68f7f14acc3efb8974d6
Vital Urbonavi <vital@fineonly.com>
2013-07-21 18:34:21 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    var text = fs.readFileSync('index.html','utf8');
+    response.send(text);
 });
 
 var port = process.env.PORT || 5000;
